8d134697b339a89d265033604fc5f6f5728ed7e8
VictorFrias25 <victorfriaz11@gmail.com>
2026-01-09 19:25:22 -0500

    Data mutation
    Added route for changing data in wojson
    tested, working
    
    able to change state of status from Opened to ready_to_close
    
    data persists.

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const fs = require('fs')
 
 const app = express()
 const port = 3001
-
+app.use(express.json())
 const woFilePath = path.join(__dirname, './db/WODB.json')
 
 app.get('/', (req, res) => res.send('Welcome to WOTracker. nav to /wo'))
 
-//work order route
+//work order list route
 app.get('/wo', (req, res) => 
     fs.readFile(woFilePath, 'utf8', (err, data) => {
         if (err) {
@@ -26,6 +26,34 @@ app.get('/wo', (req, res) =>
         }
     })
 )
+
+//work order status change route
+app.put('/wo/:wo_number', (req, res) => {
+    fs.readFile(woFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).send(`Error reading file`)
+        }
+        try {
+            const woJsonData = JSON.parse(data)
+            const woNum = req.params.wo_number
+            const updatedData = req.body
+
+            const woIndex = woJsonData.findIndex(item => item.wo_number === woNum)
+
+            if (woIndex !== -1) {
+                woJsonData[woIndex] = { ...woJsonData[woIndex], ...updatedData }
+                fs.writeFileSync(woFilePath, JSON.stringify(woJsonData, null, 2))
+                res.json(woJsonData[woIndex])
+            } else {
+                res.status(404).json({ error: `Work order not found` })
+            }
+        } catch (parseErr) {
+            console.error(parseErr)
+            res.status(500).send(`Error parsing JSON data`)
+        }
+    })
+})
 
 app.listen(port, () =>
    console.log(`Express server running @ https://localhost:${port}`)
