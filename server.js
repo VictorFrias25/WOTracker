99dada888308fed5ef49971763a3c75e6d508a18
VictorFrias25 <victorfriaz11@gmail.com>
2026-01-14 20:20:00 -0500

    Added import route to express
    using multer, added setup for import, will be using this to import csv files (and in the future more file types for work orders) to the JSON

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const multer = require('multer')
+const {parse} = require('csv-parse/sync')
 
 const app = express()
 const port = 3001
+const memStorage = multer.memoryStorage()
+const upload = multer({ storage: memStorage })
 app.use(express.json())
 app.use(express.static(path.join(__dirname, './public')))
 const woFilePath = path.join(__dirname, './db/WODB.json')
@@ -54,6 +58,44 @@ app.put('/api/wo/:wo_number', (req, res) => {
             res.status(500).send(`Error parsing JSON data`)
         }
     })
+})
+
+app.post('/api/importWO', upload.single('csvFile'), async (req, res) => {
+    try{
+        if (!req.file)
+         return res.status(400).send(`No  file uploaded.`)
+
+        const csvRaw = req.file.buffer.toString(`utf-8`)
+        const newRecords = parse(csvRaw, {
+            columns: true,
+            skip_empty_lines: true,
+            trim: true
+        })
+
+        let existingData = []
+        try{
+            const jsonRaw = await fs.readFile(woFilePath, `utf-8`)
+            existingData = JSON.parse(jsonRaw)
+        } catch (error) {
+            if (error.code !== 'ENOENT') throw error
+        }
+        const existingWO = new Set(existingData.map(item => item.wo_number))
+        const uniqueNewWO = newRecords.filter(record => !existingWO.has(record.wo_number))
+
+        if(uniqueNewWO.length === 0) 
+            return res.json({ message: `No new work orders found.`, added: 0})
+
+        const updatedJSONData = existingData.concat(uniqueNewWO)
+        await fs.writeFile(woFilePath, JSON.stringify(updatedJSONData, null, 2), `utf-8`)
+
+        res.json({
+            message: `Success, added ${uniqueNewWO.length} new workorders`,
+            ignored: newRecords.length - uniqueNewWO.length
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).send(`Error processing CSV file.`)
+    }
 })
 
 app.listen(port, () =>
